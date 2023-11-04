a5ee0fd7ffe12b5b050123fe21adb77e7546649b
martinsik <m.sikora@make.com>
2023-11-04 10:37:02 +0100

    init

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -12,16 +12,26 @@ window.addEventListener('load', (event) => {
         view: canvas,
     });
 
-    // Create the sprite and add it to the stage
-    let sprite = PIXI.Sprite.from('/assets/sample.png');
-    app.stage.addChild(sprite);
+    // create a new Sprite from an image path
+    const bunny = PIXI.Sprite.from('https://pixijs.com/assets/bunny.png');
 
-    // Add a ticker callback to move the sprite back and forth
-    // let elapsed = 0.0;
-    // app.ticker.add((delta) => {
-    //     elapsed += delta;
-    //     sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
-    // });
+    // center the sprite's anchor point
+    bunny.anchor.set(0.5);
+
+    // move the sprite to the center of the screen
+    bunny.x = app.screen.width / 2;
+    bunny.y = app.screen.height / 2;
+
+    app.stage.addChild(bunny);
+
+    // Listen for animate update
+    app.ticker.add((delta) =>
+    {
+        // just for fun, let's rotate mr rabbit a little
+        // delta is 1 if running at 100% performance
+        // creates frame-independent transformation
+        bunny.rotation += 0.1 * delta;
+    });
 
     console.log(a);
 });
